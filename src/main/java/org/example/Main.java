ef20a049f336091201977c14683935124c42b4bf
PaulMason13 <pmason1625@inst.hcpss.org>
2025-04-05 02:04:14 +0000

    sdc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,39 +1,25 @@
-package org.example; //DO NOT DELETE
+package org.example; // DO NOT DELETE
 
 import java.util.Scanner;
-import java.text.NumberFormat;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        NumberFormat percentFormat = NumberFormat.getPercentInstance();
+        Scanner s = new Scanner(System.in);
 
-        System.out.println("Enter the number of minutes spent on each of the following project tasks:");
-        System.out.print("Designing: ");
-        int designing = input.nextInt();
+        System.out.print("Enter the number of gallons: ");
+        double g = s.nextDouble();
 
-        System.out.print("Coding: ");
-        int coding = input.nextInt();
+        double q = g * 4;
+        double p = q * 2;
+        double c = p * 2;
+        double t = c * 16;
 
-        System.out.print("Debugging: ");
-        int debugging = input.nextInt();
+        System.out.println("\nIn " + g + " gallons there are:");
+        System.out.println(q + " quarts");
+        System.out.println(p + " pints");
+        System.out.println(c + " cups");
+        System.out.println(t + " tablespoons");
 
-        System.out.print("Testing: ");
-        int testing = input.nextInt();
-
-        int totalTime = designing + coding + debugging + testing;
-
-        double designingPercent = (double) designing / totalTime;
-        double codingPercent = (double) coding / totalTime;
-        double debuggingPercent = (double) debugging / totalTime;
-        double testingPercent = (double) testing / totalTime;
-
-        System.out.println("\nTask\t\t\t% Time");
-        System.out.println("Designing:\t\t" + percentFormat.format(designingPercent));
-        System.out.println("Coding:\t\t\t" + percentFormat.format(codingPercent));
-        System.out.println("Debugging:\t\t" + percentFormat.format(debuggingPercent));
-        System.out.println("Testing:\t\t" + percentFormat.format(testingPercent));
-
-        input.close();
-        }
-        }
+        s.close();
+    }
+}
